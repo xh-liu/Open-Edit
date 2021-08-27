b2ff6cd8298618794e7375b4eb32843216a3dba1
Xihui Liu <xihuiliu@ee.cuhk.edu.hk>
2021-08-27 17:28:23 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 python test.py \
     --name OpenEdit \
     --img_path test_imgs/car_blue.jpg \
-    --ori_cap 'red car' \
-    --new_cap 'blue car' \
+    --ori_cap 'blue car' \
+    --new_cap 'red car' \
     --edge_level 41 \
     --lr 0.002 \
     --which_epoch latest
